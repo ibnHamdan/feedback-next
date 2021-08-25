908dc714c01241fff94a3736cd533cb9ff030cb3
ibnHamdan <ibnHamdan@msn.com>
2021-08-25 17:15:33 +0300

    fineshed 3rd day.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,43 @@
-import '../styles/globals.css'
+import { ChakraProvider, CSSReset } from '@chakra-ui/react'
+import { Global, ThemeProvider, css } from '@emotion/react'
+import { ProvideAuth } from '@/lib/auth'
+import theme from '@/styles/theme'
+
+const GlobalStyle = ({ children }) => {
+
+
+  return (
+    <>
+    <CSSReset />
+    <Global 
+      styles={css`
+      html {
+        min-width: 360px;
+        scroll-behavior: smooth;
+      }
+
+      #__next {
+        display: flex;
+        flex-direction: column;
+        min-hight: 100vh;
+      }
+    `}
+    />
+    {children}
+    </>
+  )
+}
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <ProvideAuth>
+        <GlobalStyle />
+        
+        <Component {...pageProps} />
+        </ProvideAuth>
+    </ChakraProvider>
+    )
 }
 
 export default MyApp
---
+++ styles/theme.js
@@ -0,0 +1,23 @@
+import { extendTheme } from "@chakra-ui/react"
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+
+const fonts = {
+  body: `Inter,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`,
+  heading: `Inter,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`,
+}
+
+const fontWeights = {
+  normal: 400,
+  medium: 600,
+  bold: 700
+}
+const theme = extendTheme({ colors, fonts })
+
+export default theme; 
