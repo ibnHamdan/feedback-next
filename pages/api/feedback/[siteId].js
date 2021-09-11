024ec1eb9d0cb81e4ef0573ad355ec9b4e334eda
ibnHamdan <ibnHamdan@msn.com>
2021-09-11 22:11:16 +0300

    fixed api

--- pages/api/feedback/[siteId].js
+++ pages/api/feedback/[siteId].js
@@ -1,11 +1,10 @@
-import { getAllFeedback } from "@/lib/db-admin"
+import { getAllFeedback } from "@/lib/db-admin";
 
-
-export default async(req  , res) => {
+export default async (req, res) => {
   const siteId = req.query.siteId;
-  const {feedback, error} = await getAllFeedback(siteId);
-  if (  resutl.error ) {
-    res.status(500).jsoun({ error })
+  const { feedback, error } = await getAllFeedback(siteId);
+  if (error) {
+    res.status(500).jsoun({ error });
   }
-  res.status(200).json({feedback})
-}
+  res.status(200).json({ feedback });
+};
